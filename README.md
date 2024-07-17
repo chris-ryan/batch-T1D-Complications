7cd2fa73a574a785b099c32fbf318df1ab6b2c47
Chris Ryan <chris@codeology.com.au>
2024-07-17 10:31:56 +1000

    Added csv file output

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # batch-T1D-Complications
 An R script to apply pmtran5884's Type 1 Diabetes risk prediction tool to a batch of inputs
+
+**To use:**
+1.	Download and extract the full T1D_Complication code repository at https://github.com/pmtran5884/T1D_Complications/tree/main (Click on the Code button then select Download ZIP)
+2.	Download this repo (https://github.com/chris-ryan/batch-T1D-Complications) and extract the files into the root of the T1D_Complications directory.
+3.	Edit batch-data.csv to contain the data you wish to analyse.
+(NOTE: Do not change the column order or header labels).
+
+… in R Studio
+4.	From the top menu, select File > Open File, browse to the T1D_Complications folder and open batchT1DComplications.R
+5.	From the top menu, select Session > Set Working Directory > To Source File Location (or right-click the file name tab at the top of the Source pane to select this option)
+6.	Click the Source button to run the Script. The risks will be outputted into the Console pane and into the file output.csv.
+NOTE: If you run the script successfully multiple times, you’ll need to clear out the data rows from output.csv as it will append the results to the file.
+
--- batchT1DComplications.R
+++ batchT1DComplications.R
@@ -4,41 +4,42 @@ library(rms)
 T1Dcomp_testdata<-read.csv("test-data.csv", header = TRUE)
 
 # Import the risk models
-DPN_mod<-readRDS("data/T1Dcomp_DPNmod_logit.rds")
-DAN_mod<-readRDS("data/T1Dcomp_DANmod_logit.rds")
-DRET_mod<-readRDS("data/T1Dcomp_DRETmod_logit.rds")
-DNEP_mod<-readRDS("data/T1Dcomp_DNEPmod_logit.rds")
+DPN_mod<-readRDS("Shiny App/T1D_Complications/data/T1Dcomp_DPNmod_logit.rds")
+DAN_mod<-readRDS("Shiny App/T1D_Complications/data/T1Dcomp_DANmod_logit.rds")
+DRET_mod<-readRDS("Shiny App/T1D_Complications/data/T1Dcomp_DRETmod_logit.rds")
+DNEP_mod<-readRDS("Shiny App/T1D_Complications/data/T1Dcomp_DNEPmod_logit.rds")
 
 # Calculate and print the sum of each row
 row_sums <- rowSums(T1Dcomp_testdata)
 for (i in 1:nrow(T1Dcomp_testdata)) {
-print(paste("id: ", T1Dcomp_testdata[i,1]))
-  # predict the risk of each complication
-  print(paste("Diabetic Peripheral Neuropathy risk: ",
-        round(predict(DPN_mod,
-                      newdata=T1Dcomp_testdata[i,],
-                      na.action = na.pass,
-                      type = "fitted")*100,4),
-        "%"
-  ))
-  print(paste("Diabetic Autonomic Neuropathy risk: ",
-        round(predict(DAN_mod,
-                      newdata=T1Dcomp_testdata[i,],
-                      type = "fitted")*100,4),
-        "%"
-  ))
-  print(paste("Diabetic Retinopathy risk: ",
-              round(predict(DRET_mod,
-                            newdata=data.frame(Dr_Age=T1Dcomp_testdata[i,2],
-                                               Dx_Age=T1Dcomp_testdata[i,3],
-                                               Dur=T1Dcomp_testdata[i,2]-T1Dcomp_testdata[i,3],
-                                               AvgA1c=T1Dcomp_testdata[i,5],
-                                               avg.Systolic=T1Dcomp_testdata[i,4]),
-                            type = "fitted")*100,4),
-              "%"
-  ))
+  
+  DPN_result<-round(predict(DPN_mod,newdata=T1Dcomp_testdata[i,],
+    na.action = na.pass,
+    type = "fitted")*100,4)
+
+  DAN_result<-round(predict(DAN_mod,
+    newdata=T1Dcomp_testdata[i,],
+    type = "fitted")*100,4)
+
+  DRET_result<-round(predict(DRET_mod,
+    newdata=data.frame(Dr_Age=T1Dcomp_testdata[i,2],
+    Dx_Age=T1Dcomp_testdata[i,3],
+    Dur=T1Dcomp_testdata[i,2]-T1Dcomp_testdata[i,3],
+    AvgA1c=T1Dcomp_testdata[i,5],
+    avg.Systolic=T1Dcomp_testdata[i,4]),
+    type = "fitted")*100,4)
+  
   DNEP_result<-round(predict(DNEP_mod,
-                             newdata=T1Dcomp_testdata[i,],
-                             type = "fitted")*100,4)
-  cat("Diabetic Nephropathy risk: ",DNEP_result,"%")
+    newdata=T1Dcomp_testdata[i,],
+    type = "fitted")*100,4)
+
+  # Print the row id and risk model results out to the console
+  cat("id: ", T1Dcomp_testdata[i,1], "\n")
+  cat("Diabetic Peripheral Neuropathy risk: ",DPN_result,"% \n")
+  cat("Diabetic Autonomic Neuropathy risk: ",DAN_result,"% \n")
+  cat("Diabetic Retinopathy risk: ",DRET_result,"% \n")
+  cat("Diabetic Nephropathy risk: ",DNEP_result,"% \n \n")
+
+  # Print the results out to the csv file: output.csv
+  cat(T1Dcomp_testdata[i,1], DPN_result, DAN_result, DRET_result, DNEP_result, file="output.csv", append=TRUE, sep=",", eol="\n")
 }
